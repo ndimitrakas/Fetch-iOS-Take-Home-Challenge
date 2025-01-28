78545a0f66f8878d11374bf715460390e15d6e8d
Nickolas Dimitrakas <NICKDIMITRAKAS1@GMAIL.COM>
2025-01-28 18:30:41 -0500

    added the api service

---
+++ FetchiOSTakeHomeChallenge/FetchiOSTakeHomeChallenge/Service/APIService.swift
@@ -0,0 +1,25 @@
+//
+//  APIService.swift
+//  FetchiOSTakeHomeChallenge
+//
+//  Created by Nick Dimitrakas on 1/28/25.
+//
+
+import Foundation
+
+class APIService {
+
+    struct RecipeAPIService {
+        func fetchRecipes() async throws -> [Recipe] {
+            let url = URL(string: "https://d3jbb8n5wk0qxi.cloudfront.net/recipes.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let response = try JSONDecoder().decode(RecipeResponse.self, from: data)
+            return response.recipes
+        }
+    }
+
+    struct RecipeResponse: Decodable {
+        let recipes: [Recipe]
+    }
+    
+}
